dcede057b2b5c75b4e28b2511eb23de28042e4e8
김종국 <rgzvxw2031@gmail.com>
2019-10-24 15:20:47 +0900

    Initial Commit

---
+++ text/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  text
+//
+//  Created by 김종국 on 2019/10/24.
+//  Copyright © 2019 988KUK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
